72ccca607bbf3fb79141bc2eb2b83659135e74b6
Frederico Silva <fredericochopin@gmail.com>
2019-03-11 23:03:14 -0300

    Add Mongo DB Cloud connection

--- config-database/connection-mongo.js
+++ config-database/connection-mongo.js
@@ -1,12 +1,25 @@
 class Connection {
-  static connection(mongoose, logger){
-        try {
-            
-            mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true });
-        } catch (error) {
-         
-        }
-        
+    static connection(mongoose, logger){
+
+        const connection = {
+            "env": {
+                "MONGO_USER": "earthworm",
+                "MONGO_PASSWORD": "earthworm_p%40ss87",
+                "MONGO_DB": "events-react-dev"
+            }
+        };
+
+        const MONGO_USER = encodeURIComponent(connection.env.MONGO_USER);
+        const MONGO_PASSWORD = encodeURIComponent(connection.env.MONGO_PASSWORD);
+        const MONGO_DB = encodeURIComponent(connection.env.MONGO_DB);
+
+        mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster-virgo-8ntfm.mongodb.net/${MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
+            .then(() => {
+                console.log('Succesfully connected to the database.')
+            })
+            .catch(err => {
+                console.log('Could not connect to the database.', err);
+            });        
     }
 }
 
